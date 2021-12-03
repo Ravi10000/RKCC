7cd453715cfdb2d55ed6ff6bd864842d72a78214
Ravi Sharma <ruravi80@gmail.com>
2021-12-03 16:13:42 +0530

    make it responsive for tablets

--- script-main.js
+++ script-main.js
@@ -11,7 +11,7 @@ const popUp = document.querySelector('.pop-up')
 const contactHeading = document.querySelector('.pop-up h3')
 // const linkContainer = document.querySelectorAll('.pop-up div')
 const link = document.querySelectorAll('.pop-up a');
-const contacts = document.querySelectorAll('.popUp div')
+const contact = document.querySelectorAll('.pop-up div')
 const contactInfoContainer = document.querySelector('.contact-info')
 
 // console.log(linkContainer)
@@ -24,8 +24,11 @@ function mouseLeave(iconsList){
     });
 }
 mouseLeave(icons)
-
-
+console.log(contact);
+// link[0].addEventListener('click', ()=>{
+//     contact[0].classList.add('click')
+//     console.log(e);
+// })
 
 icons[0].addEventListener('click', e=>{
         contactHeading.innerHTML = 'Call'
@@ -47,35 +50,20 @@ icons[1].addEventListener('click', e=>{
         link[1].setAttribute('href', 'https://wa.me/+919582541150')
 })
 
-// linkContainer[0].addEventListener('click', e=>{
-//     // e.classList.add('animate')
-//     console.log(e);
-    
-// })
-// var ignoreClickOnMeElement = document.getElementById('someElementID');
 
 document.addEventListener('click', function(event) {
-    let isClickInsideElement = icons[0].contains(event.target)
-    let isClickInsideElement2 = icons[1].contains(event.target)
-    if (!isClickInsideElement && !isClickInsideElement2) {
+    const check = e => e === true;
+    const arrayOfItems = [icons[0].contains(event.target), icons[1].contains(event.target)]
+    // , link[0].contains(event.target), link[1].contains(event.target)
+
+    // let isClickInsideElement = icons[0].contains(event.target)
+    // let isClickInsideElement2 = icons[1].contains(event.target)
+    if (!arrayOfItems.some(check)) {
         console.log(event.target)
-    contactInfoContainer.classList.remove('move')
+        contactInfoContainer.classList.remove('move')
         popUp.classList.remove('show')
         icons[0].classList.remove('animate')
         icons[1].classList.remove('animate')
-
-        // event.target.classList.remove('animate')
-        
-        //Do something click is outside specified element
     }
 });
 
-// function animateOnClick(element){
-//     element.style.
-// }
-// @keyframes animate-click{
-//     0%{     transform: scale(1);   }
-//     60%{    transform: scale(.8);  }
-//     80%{    transform: scale(0.95);   }
-//     100%{   transform: scale(1);  }
-// }
